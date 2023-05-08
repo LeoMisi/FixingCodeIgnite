e65f086e958f2a8cf0b2da1a29eb62d7e97807c3
Leonardo Miranda <leonardomisi@hotmail.com>
2023-05-08 20:44:15 -0300

    Atualizando os likes dinamicos na rota PUT

--- src/index.js
+++ src/index.js
@@ -31,13 +31,14 @@ app.post("/repositories", (request, response) => {
 app.put("/repositories/:id", (request, response) => {
 	const { id } = request.params;
 	const updatedRepository = request.body;
-	updatedRepository.likes = 0;
 
 	repositoryIndex = repositories.findIndex((repository) => repository.id === id);
 
 	if (repositoryIndex < 0) {
 		return response.status(404).json({ error: "Repository not found" });
 	}
+
+	updatedRepository.likes = repositories.find((repository) => repository.id === id).likes;
 
 	const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
